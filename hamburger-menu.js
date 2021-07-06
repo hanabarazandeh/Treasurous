6e816c03a7b62d526f23f560e293b33a45451663
HanaBarazandeh <haniebarazande@gmail.com>
2021-07-06 00:58:20 -0700

    completed a fully responsive top nav bar and search, added the images for home page

---
+++ hamburger-menu.js
@@ -0,0 +1,58 @@
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_topnav
+function showMenu() {
+    var x = document.getElementById("myNav");
+    if (x.className === "topnav grid four-column add-gutters") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav grid four-column add-gutters";
+    }
+}
+
+function showSearch() {
+    console.log("showSearch called");
+    var searchInput = document.getElementById("searchInput");
+    if (searchInput.className === "search-input") {
+        searchInput.className += " responsive";
+    } else {
+        searchInput.className = "search-input";
+    }
+
+    var search = document.getElementById("search");
+    if (search.className === "search") {
+        search.className += " responsive";
+
+    } else {
+        search.className = "search";
+    }
+
+    var searchButton = document.getElementById("searchButton");
+    if (searchButton.className === "search-button") {
+        searchButton.className += " responsive";
+
+    } else {
+        searchButton.className = "search-button";
+    }
+}
+
+
+// https://www.w3schools.com/jsref/prop_element_scrolltop.asp
+// var elmnt = document.getElementById("top-bar");
+// var y = elmnt.scrollTop;
+// console.log(y);
+
+var onTopofPage = true;
+var element = document.querySelector("#top-bar")
+
+window.addEventListener("scroll", function() {
+    console.log(this.pageYOffset);
+    if (this.pageYOffset > 0) {
+        if (element.className == "top-bar grid top-3-column-fixed") {
+            element.className += " top-bar-resized";
+            onTopofPage = false;
+        }
+    } else {
+        element.className = "top-bar grid top-3-column-fixed";
+        console.log(element.className);
+    }
+
+})
